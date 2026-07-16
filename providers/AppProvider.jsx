f457c1eb8135b55f2436a2d71125335ff3f32dee
OmarVillx <u23207403@utp.edu.pe>
2026-07-16 16:07:49 -0500

    fix: agregar FeedContext.jsx faltante

--- providers/AppProvider.jsx
+++ providers/AppProvider.jsx
@@ -1,7 +1,6 @@
 import { FeedProvider } from "../context/FeedContext";
 import { LanguageProvider } from "../context/LanguageContext";
 import { ThemeProvider } from "../context/ThemeContext";
-import { FeedProvider } from "../context/FeedContext";
 
 const AppProvider = ({ children }) => {
   return (
